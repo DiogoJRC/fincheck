96dd9f1047fda12c92b2a1f7bc72ad382af0a55e
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-03-04 15:21:25 -0300

    feat: add validation with nestjs `ValidationPipe`

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  @IsString()
+  password: string;
+}
