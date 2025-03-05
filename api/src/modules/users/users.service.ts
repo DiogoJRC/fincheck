701041d6f03a76ddcc45ffdc66ca945a224bb26d
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-03-04 23:15:00 -0300

    chore: restructure folders for better organization

--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -1,7 +1,7 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaService } from 'src/database/prisma.service';
+import { PrismaService } from 'src/shared/database/prisma.service';
 import { hash } from 'bcryptjs';
 
 @Injectable()
