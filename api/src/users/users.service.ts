bcec9b322180372fb7955644c290964a4d680906
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-03-04 23:05:36 -0300

    feat: associate categories when creating a new `user`

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -13,6 +13,7 @@ export class UsersService {
 
     const emailAlreadyInUse = await this.prismaService.user.findUnique({
       where: { email },
+      select: { id: true },
     });
 
     if (emailAlreadyInUse) {
@@ -22,10 +23,37 @@ export class UsersService {
     const hashedPassword = await hash(password, 12);
 
     const user = await this.prismaService.user.create({
-      data: { name, email, password: hashedPassword },
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ],
+          },
+        },
+      },
     });
 
-    return user;
+    return {
+      name: user.name,
+      email: user.email,
+    };
   }
 
   async findAll() {
@@ -52,6 +80,7 @@ export class UsersService {
           email,
           uuid: { not: uuid },
         },
+        select: { id: true },
       });
 
       if (emailAlreadyInUse) {
