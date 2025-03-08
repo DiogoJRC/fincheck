b1d16560cd234a23cb30b0c7e84afe39f8d1c8b1
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-03-08 10:32:02 -0300

    feat: implement JWT token for authentication

--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      signOptions: { expiresIn: '7d' },
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
